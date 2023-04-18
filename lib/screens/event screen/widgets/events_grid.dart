822665e5622462506c644a5e050be4d3e554dd24
sahilsinnarkar <sahilsinnarkar@gmail.com>
2023-04-18 20:16:41 +0530

    clg

---
+++ lib/screens/event screen/widgets/events_grid.dart
@@ -0,0 +1,42 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/events.dart';
+import './event_item.dart';
+
+class EventsGrid extends StatelessWidget {
+  const EventsGrid({super.key});
+
+  Future<void> _refreshEvents(BuildContext context) async {
+    await Provider.of<Events>(context, listen: false).fetchAndSetEvents();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final eventsData = Provider.of<Events>(context);
+    final events = eventsData.items;
+
+    return RefreshIndicator(
+      onRefresh: () => _refreshEvents(context),
+      child: Scrollbar(
+        thumbVisibility: true,
+        thickness: 1,
+        child: ListView.builder(
+          physics: BouncingScrollPhysics(),
+          itemCount: events.length,
+          itemBuilder: (context, index) => ChangeNotifierProvider.value(
+            // create: (c) => events[index],
+            value: events[index],
+            child: const EventItem(
+                // id: events[index].id,
+                // name: events[index].name,
+                // imageUrl: events[index].imageUrl,
+                // date: events[index].date,
+                ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/info screen/info_screen.dart
@@ -0,0 +1,166 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/constants.dart';
+import 'package:myapp/screens/info%20screen/tabs/executive%20screen/executive_screen.dart';
+import 'package:myapp/screens/info%20screen/tabs/gallery/gallery_screen.dart';
+import 'package:myapp/screens/info%20screen/tabs/history/history_screen.dart';
+import 'component.dart';
+import 'tabs/courses/courses_screen.dart';
+
+class InfoScreen extends StatefulWidget {
+  static String routeName = 'InfoScreen';
+
+  const InfoScreen({Key? key}) : super(key: key);
+
+  @override
+  State<InfoScreen> createState() => _InfoScreenState();
+}
+
+class _InfoScreenState extends State<InfoScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kPrimaryColor,
+      body: Column(
+        children: [
+          //upper part
+          Expanded(
+            flex: 8,
+            child: Stack(
+              children: [
+                Image.asset(
+                  'assets/images/clgAbout.jpg',
+                  fit: BoxFit.cover,
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height,
+                  opacity: const AlwaysStoppedAnimation(.4),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height / 2.7,
+                  //color: kPrimaryColor,
+                  child: const Center(
+                    child: Text(
+                      "'QUOTE'",
+                      style: TextStyle(
+                        fontFamily: 'Montessart',
+                        decoration: TextDecoration.none,
+                        fontSize: 40,
+                        fontWeight: FontWeight.w400,
+                        color: kTextWhiteColor,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          //lower part
+          Expanded(
+            flex: 10,
+            child: Container(
+              color: kPrimaryColor,
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                decoration: const BoxDecoration(
+                  color: kEventItemCard,
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(25),
+                  child: Column(
+                    children: [
+                      Material(
+                        elevation: 4,
+                        color: kTextBlackColor,
+                        borderRadius: BorderRadius.circular(25),
+                        child: Container(
+                          alignment: Alignment.center,
+                          width: MediaQuery.of(context).size.width / 2,
+                          height: MediaQuery.of(context).size.height / 12,
+                          decoration: BoxDecoration(
+                            color: kPrimaryColor,
+                            borderRadius: BorderRadius.circular(25),
+                          ),
+                          child: const Text(
+                            "ABOUT GPN",
+                            style: TextStyle(
+                              fontFamily: 'Marcellus',
+                              fontSize: 25,
+                              color: kTextWhiteColor,
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height / 40,
+                      ),
+                      Expanded(
+                        child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: 1,
+                          itemBuilder: (context, index) {
+                            return Row(
+                              children: [
+                                Tabs(
+                                  imagePath: 'assets/images/executive.png',
+                                  title: 'EXECUTIVE',
+                                  onPress: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => const Exec()),
+                                    );
+                                  },
+                                ),
+                                Tabs(
+                                  imagePath: 'assets/images/courses.png',
+                                  title: 'COURSES',
+                                  onPress: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              const CoursesScreen()),
+                                    );
+                                  },
+                                ),
+                                Tabs(
+                                  imagePath: 'assets/images/history.png',
+                                  title: 'HISTORY',
+                                  onPress: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              const HistoryScreen()),
+                                    );
+                                  },
+                                ),
+                                Tabs(
+                                  imagePath: 'assets/images/gallery.png',
+                                  title: 'GALLERY',
+                                  onPress: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              const GalleryScreen()),
+                                    );
+                                  },
+                                ),
+                              ],
+                            );
+                          },
+                        ),
+                      ),
+                      //  SizedBox(height: MediaQuery.of(context).size.height/200,)
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
